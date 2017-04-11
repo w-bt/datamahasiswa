92796657f8e8135f82ad307e5d3c124355c2fd53
w-bt <widhian.bramantya@gmail.com>
2017-04-12 00:34:32 +0700

    First Commit

---
+++ DataMahasiswa/mainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace DataMahasiswa
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myDataGridView = new System.Windows.Forms.DataGridView();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textNIM = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textNama = new System.Windows.Forms.TextBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myDataGridView
+            // 
+            this.myDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myDataGridView.Location = new System.Drawing.Point(186, 12);
+            this.myDataGridView.Name = "myDataGridView";
+            this.myDataGridView.Size = new System.Drawing.Size(347, 139);
+            this.myDataGridView.TabIndex = 0;
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(57, 13);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(111, 20);
+            this.textID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NIM";
+            // 
+            // textNIM
+            // 
+            this.textNIM.Location = new System.Drawing.Point(57, 39);
+            this.textNIM.Name = "textNIM";
+            this.textNIM.Size = new System.Drawing.Size(111, 20);
+            this.textNIM.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nama";
+            // 
+            // textNama
+            // 
+            this.textNama.Location = new System.Drawing.Point(57, 65);
+            this.textNama.Name = "textNama";
+            this.textNama.Size = new System.Drawing.Size(111, 20);
+            this.textNama.TabIndex = 5;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(12, 99);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 7;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(93, 99);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(12, 128);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(93, 128);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 10;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 163);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textNama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textNIM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.myDataGridView);
+            this.Name = "mainForm";
+            this.Text = "Data Siswa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView myDataGridView;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textNIM;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textNama;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
+
---
+++ DataMahasiswa/mainForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataMahasiswa
+{
+    public partial class mainForm : Form
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=MELKA-PC;Initial Catalog=DataMHS;Integrated Security=True");
+
+        public void displayData()
+        {
+            SqlCommand qry = new SqlCommand("select * from Mahasiswa", sqlCon);
+
+            sqlCon.Open();
+            SqlDataReader rdData = qry.ExecuteReader();
+            DataTable myData = new DataTable();
+            myData.Load(rdData);
+            myDataGridView.DataSource = myData;
+            sqlCon.Close();
+        }
+
+        public mainForm()
+        {
+            InitializeComponent();
+            displayData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            // query insert untuk kolom nim dan nama
+            SqlCommand qry = new SqlCommand("insert into Mahasiswa (nim, nama) values ('"+textNIM.Text+"','"+textNama.Text+"')", sqlCon);
+            sqlCon.Open();
+            qry.ExecuteNonQuery();
+            sqlCon.Close();
+            // tampilkan pada myDataGridView setelah insert
+            displayData();
+            // tampilkan pesan bahwa data berhasil ditambahkan
+            MessageBox.Show("Data sukses ditambahkan!");
+            // bersihkan semua textbox
+            textID.Clear();
+            textNIM.Clear();
+            textNama.Clear();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            // query update untuk kolom nim dan nama berdasarkan ID
+            SqlCommand qry = new SqlCommand("update Mahasiswa set nim='"+textNIM.Text+"', nama='"+textNama.Text+"' where mhsTableID='"+textID.Text+"'", sqlCon);
+            sqlCon.Open();
+            qry.ExecuteNonQuery();
+            sqlCon.Close();
+            // tampilkan pada myDataGridView setelah update
+            displayData();
+            // tampilkan pesan bahwa data berhasil diubah
+            MessageBox.Show("Data " + textID.Text + " sukses diubah!");
+            // bersihkan semua textbox
+            textID.Clear();
+            textNIM.Clear();
+            textNama.Clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            // query delete berdasarkan ID
+            SqlCommand qry = new SqlCommand("delete from Mahasiswa where mhsTableID='" + textID.Text + "'", sqlCon);
+            sqlCon.Open();
+            qry.ExecuteNonQuery();
+            sqlCon.Close();
+            // tampilkan pada myDataGridView setelah delete
+            displayData();
+            // tampilkan pesan bahwa data berhasil dihapus
+            MessageBox.Show("Data " + textID.Text + " sukses dihapus!");
+            // bersihkan semua textbox
+            textID.Clear();
+            textNIM.Clear();
+            textNama.Clear();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            // query select berdasarkan nama
+            SqlCommand qry = new SqlCommand("select * from Mahasiswa where nama LIKE '%" + textNama.Text + "%'", sqlCon);
+
+            sqlCon.Open();
+            SqlDataReader rdData = qry.ExecuteReader();
+            DataTable myData = new DataTable();
+            myData.Load(rdData);
+            myDataGridView.DataSource = myData;
+            sqlCon.Close();
+            // tampilkan pesan bahwa pencarian selesai
+            MessageBox.Show("Pencarian selesai!");
+            // bersihkan semua textbox
+            textID.Clear();
+            textNIM.Clear();
+            textNama.Clear();
+        }
+    }
+}
